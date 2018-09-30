14b0b16e4e9d0dc225168bf59563036d3a717416
nitishk72 <nitishk72@gmail.com>
2018-09-30 18:27:47 +0530

    form registraion

--- src/app/registraion/registraion.component.ts
+++ src/app/registraion/registraion.component.ts
@@ -1,42 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { auth } from 'firebase';
-import { NgForm } from '@angular/forms';
-
+import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "angularfire2/auth";
+import { auth } from "firebase";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-registraion',
-  templateUrl: './registraion.component.html',
-  styleUrls: ['./registraion.component.css']
+  selector: "app-registraion",
+  templateUrl: "./registraion.component.html",
+  styleUrls: ["./registraion.component.css"]
 })
 export class RegistraionComponent implements OnInit {
-
-  isRegistered : boolean = false;
-  isFormFilled : boolean = false;
-  isAuth : boolean = false;
-  constructor(public af: AngularFireAuth,) {
-    af.authState.subscribe(
-      (user) => {
-        if (user != null) {
-          this.isAuth = true;
-          console.log(user.email)
-        }else{
-          this.isAuth = false;
-          console.log('logged out');
-        }
-      });
-   }
-
-  ngOnInit() {
+  step: number = 0;
+  isRegistered: boolean = false;
+  isFormFilled: boolean = false;
+  isAuth: boolean = false;
+  constructor(public af: AngularFireAuth) {
+    af.authState.subscribe(user => {
+      if (user != null) {
+        this.isAuth = true;
+      } else {
+        this.isAuth = false;
+      }
+    });
   }
 
-  login(){
-    this.af.auth.signInWithPopup(new auth.GoogleAuthProvider);
+  ngOnInit() {}
+
+  login() {
+    this.af.auth.signInWithPopup(new auth.GoogleAuthProvider());
   }
-  logout(){
+  logout() {
     this.af.auth.signOut();
   }
-  register(form: NgForm){
-    console.log(form)
+  register() {
+    console.log('submitted');
+  }
+  next(){
+    this.step++;
+  }
+  prev(){
+    this.step--;
   }
 }
